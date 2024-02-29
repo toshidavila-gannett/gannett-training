075abf70ad27752911a10720e6d8813539ebd274
Toshi Dávila <adavila@gannett.com>
2024-02-29 16:17:10 -0500

    jest training

---
+++ sum.test.js
@@ -0,0 +1,40 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  
+  test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
+
+  function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK!');
+  }
+  
+  test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+  
+    // You can also use a string that must be contained in the error message or a regexp
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
+  
+    // Or you can match an exact error message using a regexp like below
+    expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/); // Test fails
+    expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/); // Test pass
+  });
